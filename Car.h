99a420c4470fe506d495a9ae2b5a90b835380be9
FlzGlz <51001772+richfls@users.noreply.github.com>
2023-03-21 09:29:44 -0600

    Update Car.h

--- Car.h
+++ Car.h
@@ -8,9 +8,13 @@ private:
 	float xpos;
 	float ypos;
 	int green = rand() % 100 + 200;
-	int green = rand() % 100 + 200;
-	int dircetion = RIGHT;
-public:
-	car(int xpos,int ypos,int direction);
-};
+	int blue = rand() % 100 + 200;
+	int direction = RIGHT;
 
+public:
+	Car(int xpos,int ypos,int direction);
+	void draw(sf::RenderWindow& window);
+	void move();
+	bool collide(int x, int y);
+	void printinfo() { cout << "I am a car and position is" << xpos << "," << ypos << endl; }
+};
